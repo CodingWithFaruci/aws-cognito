cee30a493fc6104f710807cb946cca1cdd7d9a9c
FarukAda <faruk88ada@gmail.com>
2023-06-25 22:15:24 +0200

    Initial commit

---
+++ test/aws-cognito.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCognito from '../lib/aws-cognito-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cognito-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCognito.AwsCognitoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
